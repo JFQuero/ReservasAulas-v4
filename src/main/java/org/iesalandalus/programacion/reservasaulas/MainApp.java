cd9bfe6f1de1a788b2ec4bd67a14f20cac137f7d
JFQueroDev <JFQueroDev@Base-LPTP>
2019-05-01 22:24:50 +0200

    Clonado el repositorio de Github
    Copiada la estructura del proyecto anterior
    
    Cambiado el nombre del alumno en Readme.md

--- src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
@@ -1,19 +1,23 @@
 package org.iesalandalus.programacion.reservasaulas;
-/*
+
 import org.iesalandalus.programacion.reservasaulas.controlador.ControladorReservasAulas;
+
 import org.iesalandalus.programacion.reservasaulas.controlador.IControladorReservasAulas;
 import org.iesalandalus.programacion.reservasaulas.modelo.IModeloReservasAulas;
 import org.iesalandalus.programacion.reservasaulas.modelo.ModeloReservasAulas;
+import org.iesalandalus.programacion.reservasaulas.vista.Consola;
 import org.iesalandalus.programacion.reservasaulas.vista.IVistaReservasAulas;
 import org.iesalandalus.programacion.reservasaulas.vista.VistaReservasAulas;
-*/
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		/*IVistaReservasAulas vista = new VistaReservasAulas();
+		Consola.mostrarCabecera("Programa para la gestión de reservas de aulas del IES Al-Ándalus");
+		Consola.mostrarCabecera("Juan Fernández Quero - 1ºDAM");
+		IVistaReservasAulas vista = new VistaReservasAulas();
 		IModeloReservasAulas modelo = new ModeloReservasAulas();
-		IControladorReservasAulas controlador = new ControladorReservasAulas(modelo, vista);
-		controlador.comenzar();*/
+		IControladorReservasAulas controlador = new ControladorReservasAulas(vista, modelo);
+		controlador.comenzar();
 	}
 
 }
